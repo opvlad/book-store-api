47b939c183617f61cf78359f786eda3d9755ca65
opvlad <opvladq@gmail.com>
2026-03-30 14:01:45 +0000

    feat: add authors endpoints

---
+++ app/routers/v1/authors.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends
+
+from app.dependencies import sessionDep, get_current_admin
+from app.schemas import (
+    AuthorResponse,
+    AuthorListPaginatedResponse,
+    AuthorCreate,
+    AuthorUpdate,
+)
+from app.models import User
+from app.services import (
+    get_author as service_get_author,
+    get_authors as service_get_authors,
+    create_author as service_create_author,
+    update_author as service_update_author,
+    delete_author as service_delete_author,
+)
+
+
+router = APIRouter()
+
+
+@router.get("", response_model=AuthorListPaginatedResponse)
+async def authors_list(db: sessionDep, limit: int = 100, offset: int = 0):
+    total, items = await service_get_authors(db, limit=limit, offset=offset)
+    return {"total": total, "limit": limit, "offset": offset, "items": items}
+
+
+@router.get("/{author_id}", response_model=AuthorResponse)
+async def read_author_details(db: sessionDep, author_id: int):
+    return await service_get_author(db, author_id)
+
+
+@router.post("", response_model=AuthorResponse)
+async def create_author(
+    db: sessionDep, author: AuthorCreate, _: User = Depends(get_current_admin)
+):
+    return await service_create_author(db, author)
+
+
+@router.patch("/{author_id}", response_model=AuthorResponse)
+async def modify_author(
+    db: sessionDep,
+    author_id: int,
+    author_update: AuthorUpdate,
+    _: User = Depends(get_current_admin),
+):
+    return await service_update_author(db, author_id, author_update)
+
+
+@router.delete("/{author_id}", status_code=204)
+async def delete_author(
+    db: sessionDep, author_id: int, _: User = Depends(get_current_admin)
+):
+    await service_delete_author(db, author_id)
